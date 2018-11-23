f09e72ea90d797d63d21f7525b0548867e8c5e46
hossain24 <haj.ctg@gmail.com>
2018-11-23 01:06:42 +0100

    deployed to the cloud corrected

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -4,15 +4,17 @@ import "firebase/auth";
 
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyDFz11cF3k-1l9fRH1HIJpA2uwODxh-yWE",
-  authDomain: "hill-track-11d1d.firebaseapp.com",
-  databaseURL: "https://hill-track-11d1d.firebaseio.com",
-  projectId: "hill-track-11d1d",
-  storageBucket: "hill-track-11d1d.appspot.com",
-  messagingSenderId: "1048459968019"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: ""
 };
 
 firebase.initializeApp(config);
-firebase.firestore().settings({ timestampsInSnapshots: true });
+firebase.firestore().settings({
+  timestampsInSnapshots: true
+});
 
 export default firebase;
